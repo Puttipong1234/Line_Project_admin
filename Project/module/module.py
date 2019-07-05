7dcba5e87289f99a3bad18b31c3642126decb1a5
Puttipong1234 <46212403+Puttipong1234@users.noreply.github.com>
2019-07-05 13:04:57 +0700

    "INITIAL CODING MODULE'

---
+++ Project/module/module.py
@@ -0,0 +1,24 @@
+from linebot.models import *
+
+def PickFileCategory():
+    pass
+
+def ReviewAllFile():
+    pass
+
+def GetFile():
+    pass
+
+
+def UploadFile():
+    pass
+
+def DeleteFile():
+    pass
+
+
+    
+
+
+
+
